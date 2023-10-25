21ea1e37be573d5d1d64c7abb7dba8f3a839b7b6
guerrerokai <20-04089@g.batstate-u.edu.ph>
2023-10-25 15:36:21 +0800

    updated 10/25/23

--- src/app/view-post/view-post.component.ts
+++ src/app/view-post/view-post.component.ts
@@ -13,6 +13,8 @@ export class ViewPostComponent {
   rating:any;
   postID:any
 
+  
+
   constructor(
     private route: ActivatedRoute,
     public fireservice:FireServiceService
@@ -24,7 +26,14 @@ export class ViewPostComponent {
     this.route.paramMap.subscribe(params => {
       this.postID = params.get('postID');
       // Use this.productId to fetch and display product details
+      this.navigationLinks.forEach((link:any) => {
+        link.addEventListener("click", this.handleNavigationClick);
+      });
+        this.showSection("blogs");
+        this.navigationLinks[0].classList.add("active"); 
     });
+  
+    
 
     console.log(this.postID.toString());
     this.getPost();
@@ -79,6 +88,45 @@ export class ViewPostComponent {
   move(event: google.maps.MapMouseEvent) {
     if (event.latLng != null) this.display = event.latLng.toJSON();
   }
+
+  showSection(sectionId:any) {
+    const sections = document.querySelectorAll(".content-section");
+    
+    
+    sections.forEach((section) => {
+      if(section instanceof HTMLElement){
+        section.style.display = "none";
+      }
+    });
+
+    const selectedSection = document.getElementById(sectionId);
+    if (selectedSection) {
+        selectedSection.style.display = "block";
+    }
+
+  }
+
+
+
+  handleNavigationClick = (event:any) => {
+    const targetId = event.target.getAttribute("data-section");
+    if (targetId) {
+        this.showSection(targetId);
+
+        // Remove the "active" class from all navigation links
+        this.navigationLinks.forEach((link:any) => {
+            link.classList.remove("active");
+        });
+
+        // Add the "active" class to the clicked link
+        event.target.classList.add("active");
+    }
+}
+
+  photos = document.querySelectorAll('.zoomable');
+  modal = document.getElementById('myModal');
+  zoomedImg = document.getElementById('zoomedImg') as HTMLImageElement;
+  navigationLinks = document.querySelectorAll("nav ul li a");
 
 
 }
